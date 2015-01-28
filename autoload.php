cac665dce1393c067fb3ffe4347f6e0b01dda8bf
Thomas Müller <thomas.mueller@tmit.eu>
2015-01-28 22:06:02 +0100

    Adding nikic/php-parser 1.1.0

--- autoload.php
+++ autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite74b4316c3262a1612fa2917f82871a7::getLoader();
+return ComposerAutoloaderInit7a821d325750316758e15ee27ae980f9::getLoader();
